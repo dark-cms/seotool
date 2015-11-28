1c549597b6d0528a3ae4ba07239d7da6be5ae78a
Damian Schwyrz <Damian89@users.noreply.github.com>
2015-11-28 21:56:00 +0100

    Update settings.php for Verson 1.0
    Removed unused settings, added standard admin credentials

--- app/settings.php
+++ app/settings.php
@@ -1,13 +1,16 @@
 <?php
 
+define('USER', 'admin');
+define('PASS', '123456');
+
 return [
     'settings' => [
         // DB settings
         'database' => [
             'host'     => 'localhost',
-            'username' => 'c1_tool',
+            'username' => '',
             'password' => '',
-            'db'       => 'c1_tool',
+            'db'       => '',
             'port'     => 3306,
             'prefix'   => 'st_',
             'charset'  => 'utf8',
@@ -16,18 +19,10 @@ return [
         'view'     => [
             'path' => __DIR__ . '/templates/'
         ],
-        // Caching setting
-        'cache'    => [
-            'path'           => __DIR__ . '/../cache',
-            'cacheActive'    => false,
-            'compressActive' => false,
-            'lifetime'       => 1,
-            'debug'          => true,
-            'whitelist'      => []
-        ],
         // Slim Settings
         'settings' => [
             'displayErrorDetails' => true,
         ],
     ],
 ];
+
